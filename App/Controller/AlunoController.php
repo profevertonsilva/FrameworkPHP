07248e74c7425576473ea6442d201d5724f0a321
evertonrafaelifsp <evertonrafael@ifsp.edu.br>
2025-03-20 14:37:28 -0300

    Correção do Arquivo AlunosController.php

---
+++ App/Controller/AlunoController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use FW\Controller\Action;
+use App\DAO\AlunosDAO;
+use App\Model\AlunosModel;
+
+
+class AlunosController extends Action{
+
+    public function index(){
+        $title = "alunos";
+        $title_pagina = "Alunos";
+
+        $this->getView()->title = $title;
+        $this->getView()->title_pagina = $title_pagina;
+
+        $this->render('index', 'dashboard');
+    }
+    
+    
+    
+
+    public function validaAutenticacao() {
+
+        
+    }
+}
